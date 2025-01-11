69282fc2566c61a4c6551ba2dbe3f9ebca48e676
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-11 20:55:54 +0530

    update the code

--- partices/arithmeticOperator/Arithmaticoperator.py
+++ partices/arithmeticOperator/Arithmaticoperator.py
@@ -1,13 +1,38 @@
 #arithmetic operator sample python codes.
 ##arithmetic 
 print(1*2+9*5/90-12)
-#calculate BMI python code
-weight/height*height
-
 ##comparsion
-##logical
+num1 = int(input("enter the num1 :"))
+num2 = int(input("enter the num2 :"))
+print(num1 ==num2)
+print(num1 !=num2)
+print(num1 >num2)
+print(num1 <num2)
+print(num1 >=num2)
+print(num1 <=num2)
+
+##logical and ,or ,not
+#method 1
+x = bool(input("enter the x :"))
+y = bool(input("enter the y :"))
+print(x and y)
+print(x or y)
+print('x',not x,'y',not y)
+#method 2
+x = int(input("enter the x :"))
+y = int(input("enter the y :"))
+print(x and y)
+print(x or y)
+print('x',not x,'y',not y)
+
 ##bitwise
+
 ##floor division
+a = int(input("enter the a :"))
+b = int(input("enter the b :"))
+print('floor division operator:',a//b)
 ##assignment
+
 ##identity
+
 ##membership
