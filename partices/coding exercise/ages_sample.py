b61b6f4ba181ff65e4ba6c02cbb41b4b9263ac4c
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-20 22:30:37 +0530

    sample if else code

---
+++ partices/coding exercise/ages_sample.py
@@ -0,0 +1,8 @@
+#sample if-else python code
+height =int(input("enter heigth in feet :"))
+if height>=3 :
+    print("buy token")
+elif height>=0 and height<=2:
+    print("no need to give token")
+else :
+    print("invalid input")
