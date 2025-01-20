e61a20ec8a8e5ce657777a729390fd12f6f67f89
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-20 21:53:38 +0530

    updated the code

--- partices/fstrings/f_strings.py
+++ partices/fstrings/f_strings.py
@@ -3,3 +3,4 @@ name = "gandla bhargavi"
 age =32
 height = 5.3
 print(f"my name is {name},i am { age} years old and my height is {height} inches")
+print(f"my name is {name},i am { age*2} years old and my height is {height} inches")
