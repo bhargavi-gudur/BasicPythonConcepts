a99233baaecf96fca5f3ab6191bcae07c1288997
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-25 19:36:05 +0530

    leap year coding partice python code

---
+++ partices/coding exercise/leapyear.py
@@ -0,0 +1,12 @@
+#sample program wheather year is leap year or not
+year = int(input("which year you want to check "))
+if year % 4 == 0:
+    if year %100 == 0:
+       if year %400 == 0: 
+        print(f"{year} is a leap year")
+       else :
+        print(f"{year} is not a leap year")
+    else :
+     print(f"{year} is a leap year")
+else :
+ print(f"{year} is not a leap year")
