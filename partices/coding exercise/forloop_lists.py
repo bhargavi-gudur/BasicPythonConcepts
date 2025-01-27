3067cfe13bcb0ba901180c13d1425ee308fc4b57
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-27 17:17:10 +0530

    updated forloop  python code

---
+++ partices/coding exercise/forloop_lists.py
@@ -0,0 +1,20 @@
+#sample list python program
+exp = [2340,2500,3100,2980]
+total = 0
+for item in exp:
+    total = total+ item
+print("item :", total)
+# sample list using range function 
+for i in  range(1,10):
+    print("i:",i)
+# sample list program using range function add.
+add =0
+for i in  range(1,10):
+    add = add+i
+    print("i:",i," add:",add)  
+# sample list using range function and len function.
+# addition 
+lists_item =[1,2,3,4,5,6,7,8,9]
+for i in  range(len(lists_item)):
+    print("lists ",lists_item[i]+i)
+    
