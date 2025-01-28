dd8780a1c057d847a0d7b9e4b9635a7327312b02
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-28 18:04:18 +0530

    address code using json library

---
+++ json/addressbook.py
@@ -0,0 +1,30 @@
+# we write two programs
+
+# to create address book and write some records into it
+# read this book
+book ={}
+book ['tom']={
+    'name':"tom",
+    "address":"1 red street,NY",
+    'PHONE': 67899089887
+}
+book ['bon']={
+    'name':"bon",
+    "address":"1 red street,NY",
+    'PHONE': 23447567
+}
+
+# import json in built function
+import json
+s = json.dumps(book,indent= 4)
+#print("string :",s)
+#write the json file
+with open(r"C:\Users\gandl\Downloads\git_bhargavi_gudur\BasicPythonConcepts\json\data\data.txt","w") as f:
+   data = f.write(s)
+
+print("data:",data)
+#read the json file
+with open(r"C:\Users\gandl\Downloads\git_bhargavi_gudur\BasicPythonConcepts\json\data\data.txt","r") as f:
+   data = json.load(f)
+
+print("data:",data)
