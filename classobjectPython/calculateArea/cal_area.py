53a032d566e6a0a0c11aa6f4960642d566a2ee28
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-30 22:48:07 +0530

    class aand object

---
+++ classobjectPython/calculateArea/cal_area.py
@@ -0,0 +1,10 @@
+# sample main python program
+def calculate_area(base,height):
+  print("__name__:",__name__)
+  return 1/2*(base*height)
+
+if __name__ =="__main__":
+  print("i am in calculate area .py")
+  a = calculate_area(10,20)
+  print("area : ",a)
+
---
+++ classobjectPython/calculateArea/caller.py
@@ -0,0 +1,3 @@
+import cal_area as area
+print("i am in caller.py")
+area.calculate_area(5,10)
