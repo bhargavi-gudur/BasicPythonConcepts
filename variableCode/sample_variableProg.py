971af768f8769f317c1e33eddf5df1c8fdbc58e2
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-31 12:06:35 +0530

    sample variable code

---
+++ variableCode/sample_variableProg.py
@@ -0,0 +1,18 @@
+# Write Python 3 code in this vscode editor and run it.
+#sample variable code.
+print("Try variable program")
+x =56
+print("x:",56)
+y=z=8
+z=x+y
+print("y:",y,"z:",z)
+# h-[0],e-[1],l[2],l[3],o[4] ,
+# space [6]
+# w[6],o[7],r[8l,d[9],![10]
+string_name ="hello world!"
+print(string_name[0])
+print(string_name[1:])
+print(string_name[1:3])
+print(string_name[:4])
+print(string_name[-1:])
+print(string_name[:-1])
