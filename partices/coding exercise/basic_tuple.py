5767befab3652b412d53f4f29a709269567af17d
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-31 12:15:03 +0530

    tuple update , and basic tuple python

---
+++ partices/coding exercise/basic_tuple.py
@@ -0,0 +1,19 @@
+# Write Python 3 code in this vscode editor and run it.
+# sample tuple code.
+nums = (2,3,4,56,67,89,123,129)
+# type function
+print("type : ",type(nums))
+for i in nums:
+    print("nums",i)
+#lenght function    
+print("lenght:",len(nums))
+
+print(nums[2:])
+print(nums[2:6])
+print(nums[-2:])
+# iteration using for loop
+for i in nums[:-2]:
+  print("nums[-2:0]",i)
+ 
+name ="bhargavi"
+print("name:",name[-3])
