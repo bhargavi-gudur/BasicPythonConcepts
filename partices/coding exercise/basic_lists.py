a7d5f1cac224c3279d3333d4773f557e5eba1d91
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-31 12:06:17 +0530

    basic list code

---
+++ partices/coding exercise/basic_lists.py
@@ -0,0 +1,17 @@
+# Write Python 3 code in this vscode editor and run it.
+# sample list code.
+nums = [2,3,4,56,67,89,123,129]
+for i in nums:
+    print("nums",i)
+    
+print("lenght:",len(nums))
+
+print(nums[2:])
+print(nums[2:6])
+print(nums[-2:])
+
+for i in nums[:-2]:
+  print("nums[-2:0]",i)
+ 
+name ="bhargavi"
+print("name:",name[-3])
