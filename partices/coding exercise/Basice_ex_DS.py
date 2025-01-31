7ed75798bced53134d75741e765a07329c1c549f
Gandla Bhargavi <barau.avril@gmail.com>
2025-01-31 12:46:18 +0530

    basic example ds (lists,tuple,set,dictionarty)

---
+++ partices/coding exercise/Basice_ex_DS.py
@@ -0,0 +1,26 @@
+#lists
+#Real-World Use Case: Managing inventory in an online store.
+fruits = ["apple", "banana", "cherry", "date"]
+print(fruits)
+fruits.append("elderberry")
+print(fruits)
+#tuple
+#Real-World Use Case: Storing GPS coordinates of a specific location.
+coordinates = (34.0522, -118.2437)
+print(coordinates)
+#Dictionary
+#Real-World Use Case: Storing student names and their grades.
+student_grades = {"Alice": "A", "Bob": "B", "Charlie": "C"}
+print(student_grades)
+print(student_grades["Alice"])
+#Set:
+#Real-World Use Case: Ensuring unique usernames in a registration system.
+unique_numbers = {1, 2, 3, 4, 5}
+print(unique_numbers)
+unique_numbers.add(6)
+print(unique_numbers)
+
+
+
+
+
