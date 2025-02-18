bedfba233c399b237356f3cdcf713aa565ea42b8
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-18 22:48:38 +0530

    funcation program

---
+++ function/fun.py
@@ -0,0 +1,14 @@
+# sample  function python.
+# to exectue the function block which perform 
+#  the specific task
+def greet():
+    print("hi")
+    print("welcome to python learning")
+def add():
+    a= int(input("enter the value a"))
+    b= int(input("enter the value b"))
+    c =a+b
+    print("addition :",c)
+    
+greet()
+add()
