074bc315d2a5e6f961e8d9893af98b32e9898e3b
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-22 20:37:18 +0530

    add .editorconfig and VSCode settings; implement sample arithmetic operations in Python

---
+++ numbers/aritmaticOperator.py
@@ -0,0 +1,24 @@
+'''
+sample numbers using arithmatic operator in python 
+'''
+# take input from user
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+
+# arithmatic operations
+addition = first_num + second_num
+subtraction = first_num - second_num
+multiplication = first_num * second_num
+division = first_num / second_num
+modulus = first_num % second_num
+exponent = first_num ** second_num
+floor_division = first_num // second_num
+
+# print the result
+print("Addition of two numbers is: ", addition)
+print("Subtraction of two numbers is: ", subtraction)
+print("Multiplication of two numbers is: ", multiplication)
+print("Division of two numbers is: ", division)
+print("Modulus of two numbers is: ", modulus)
+print("Exponent of two numbers is: ", exponent)
+print("Floor division of two numbers is: ", floor_division)
