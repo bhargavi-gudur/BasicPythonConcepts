deef9909398a51c0acb42fdf4bf7936ae3d4b4b2
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-24 23:00:02 +0530

    add sample code for using len() function with a list in Python

---
+++ inbuiltMethods/len_fun.py
@@ -0,0 +1,6 @@
+# finding the lenght of acollection.
+#example 1
+fruits = ["apple","banana","cherry","strawberry","grape"]
+print(len(fruits))
+for i in range(len(fruits)):
+    print(f"{i}->{fruits[i]}")
