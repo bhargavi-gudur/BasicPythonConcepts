030f320e34e21fa633586df9a13e79c2f5ec57ce
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-25 22:19:12 +0530

    add sample code for using inbuilt math functions in Python

---
+++ numbers/math_inbuilt_fun.py
@@ -0,0 +1,14 @@
+#sample inbuilt math functions in python.
+import math
+
+print("first :",round(2.9),"second:",round(2.3))
+print(abs(-3.9))
+print("first :",math.ceil(2.9),"second :",math.ceil(2.3))
+print("first :",math.floor(2.9),"second :",math.floor(2.3))
+print("square :",math.sqrt(16))
+print("power :",math.pow(2,3))
+print("pi :",math.pi)
+
+
+
+
