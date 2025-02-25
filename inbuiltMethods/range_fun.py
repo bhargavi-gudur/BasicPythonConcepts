e2ec077df8ae82fcd71c4c309c1ca42913bcef94
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-25 12:10:49 +0530

    add sample code for using range() function in Python

---
+++ inbuiltMethods/range_fun.py
@@ -0,0 +1,15 @@
+# generating sequences of numbers.
+# example 1:
+print("****************************")
+for i in range(5):
+    print(f"i{i} :" ,i)
+print("****************************")
+# example 2:
+for i in range(2,14,3):
+    print(f"i{i} :" ,i)
+print("****************************")
+
+# used when you need to iterate over a sequence of numbers.
+# typically used in loops
+# it generates numbers from starting point(default 0) upto
+# (not including) the end point.
