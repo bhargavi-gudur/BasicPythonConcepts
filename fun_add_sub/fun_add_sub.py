dfb264817e35d6751e7fa10a4f4f2abed4bbc35a
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-28 13:12:36 +0530

    add sample code for performing addition and subtraction using functions

---
+++ fun_add_sub/fun_add_sub.py
@@ -0,0 +1,13 @@
+#
+a =int(input("enter the a value : "))
+b =int(input("enter the b value : "))
+
+def addition(first_num,second_num):
+    addition = first_num+second_num
+    print("addition :",addition)
+def sub(first_num,second_num):
+    subraction = first_num-second_num
+    print("subraction :",subraction)
+    
+addition(a,b)
+sub(a,b)
