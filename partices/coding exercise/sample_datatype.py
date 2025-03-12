c58f56664e33907f0631a3a574f456c53a61f13c
Gandla Bhargavi <barau.avril@gmail.com>
2025-03-12 12:34:16 +0530

    add sample code for demonstrating various data types in Python

---
+++ partices/coding exercise/sample_datatype.py
@@ -0,0 +1,26 @@
+#sample numeric data type 
+num =2.5
+print("the datatype: ",type(num))
+comp =2.5+6j
+print("the datatype: ",type(comp))
+# example 1
+b =6
+c =6
+a = b<c
+print("the datatype: ",type(a))
+#sequence  example
+lst =[2,3,4,6,7,78]
+print("the datatype: ",type(lst))
+#tuple example 
+tup =(2,3,4,6,7,78)
+print("the datatype: ",type(tup))
+#string example 
+string ="bhargavi"
+print("the datatype: ",type(string))
+#example range 
+range_num = range(10)
+print("range values : ",range_num)
+
+for range_num in range(10):
+    print("num: ",range_num)
+
