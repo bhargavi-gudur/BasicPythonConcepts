1201af3b8966da01af8c2ed82a01d517829320f9
Gandla Bhargavi <barau.avril@gmail.com>
2025-03-30 17:41:25 +0530

    add examples demonstrating the usage of the round function with various data types

--- round_function/round_fun.py
+++ round_function/round_fun.py
@@ -17,3 +17,16 @@ print(round(675,1))
 print(round(675.678889,1))
 print(round(675.9,-1))
 print("type :  ",type(round(675.9,-1)))
+#  round function with float and int
+print(round(7.49864, 2))
+print(round(7.5, 1))
+print(round(float(7), 2))
+print(round(int(7.49864), 2))
+print(round(7.9864, 0))
+print(round(6.75, 1))
+print(round(6.85, 2))
+print(round(674.1012, -1))
+print(round(1212, -1))
+# round function with string
+print(round("7.49864", 2))  # TypeError: type str not supported
+print(round("7.5", 1))  # TypeError: type str not supported
