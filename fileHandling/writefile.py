8542b5bd33d16a585c9922e5a91bd9b15138b287
Gandla Bhargavi <barau.avril@gmail.com>
2025-04-30 12:58:16 +0530

    Add file copying functionality and verify copy success in writefile.py

--- fileHandling/writefile.py
+++ fileHandling/writefile.py
@@ -12,3 +12,18 @@ f.close()
 f = open('text.txt', 'r')
 print(f.read())
 f.close()
+# To copy a file in Python
+readfile= open("output.txt","r")
+writefile =open("copy.txt","w")
+for lines in readfile:
+    writefile.write(lines)
+readfile.close()
+writefile.close()
+
+# To check if the file is copied successfully
+check_read = open("copy.txt","r")
+check_read = open("copy.txt", "a+")
+
+check_read.write("This is a new line added to the copied file.\n")
+print(check_read.read()) 
+check_read.close()    
