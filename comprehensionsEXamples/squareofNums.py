79af5cd93bd245430a7e3a5ab44d18d5d5bd57d6
Gandla Bhargavi <barau.avril@gmail.com>
2025-05-04 11:04:52 +0530

    Add squaresofNums.py to calculate and print squares of a given range

---
+++ comprehensionsEXamples/squareofNums.py
@@ -0,0 +1,8 @@
+# squaresofNums.py
+nums =int(input("enter the number of elements in the list "))
+def squaresofNums(nums):
+    squares = [X**2 for X in range(nums)]
+    print(squares)
+    
+squaresofNums(nums)
+   
