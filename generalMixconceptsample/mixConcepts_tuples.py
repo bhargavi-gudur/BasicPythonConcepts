a94ae36e2d01f861a88e675c557eba1c22f66ae7
Gandla Bhargavi <barau.avril@gmail.com>
2025-05-07 12:28:00 +0530

    Add mixConcepts_tuples.py for tuple and list operations demonstration

---
+++ generalMixconceptsample/mixConcepts_tuples.py
@@ -0,0 +1,10 @@
+tuple_var = [100, 200, 300]
+print(tuple(tuple_var))
+
+list_var = [100, 200, 300, 209, 12, 23]
+
+del list_var[2]
+print(tuple(list_var))
+
+data = (10, 20, 30, 40, 50, 60, 50)
+print(data.index(50))
