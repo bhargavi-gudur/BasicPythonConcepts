f660383b0cca527f1335de34c6ed5e773e186778
Gandla Bhargavi <40709879+bhargavi-gudur@users.noreply.github.com>
2025-12-11 22:53:07 +0530

    Update ArithmaticAdd.py

--- ArithmeticAdd/ArithmaticAdd.py
+++ ArithmeticAdd/ArithmaticAdd.py
@@ -1,10 +1,13 @@
 # program to add digits of a number.
+
 # method 1 S
 num = input("enter the integer : ")
 digit1 = int(num[0])
 digit2 = int(num[1])
 sum = digit1+ digit2
 print('the add digit addition :',sum)
+
+
 # method 2 using for loop
 num = input("enter the integer : ")
 sum = 0
